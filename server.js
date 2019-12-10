4da96e31b1fde75468e62c6b3306b0006d9350ef
bsjherbert <56240025+bsjherbert@users.noreply.github.com>
2019-12-10 15:16:47 -0500

    added gitignore and jaws connection

--- server.js
+++ server.js
@@ -12,15 +12,21 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Waynesworld13!",
-  database: "burgers_db"
-});
+var connection;
 
-connection.connect(function (err) {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Waynesworld13!",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -28,8 +34,8 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-app.get("/", function (req, res) {
-  connection.query("SELECT * FROM burgerTable;", function (err, data) {
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgerTable;", function(err, data) {
     if (err) {
       return res.status(500).end();
     }
@@ -38,41 +44,38 @@ app.get("/", function (req, res) {
   });
 });
 
-app.post("/api/burger", function (req, res) {
-  connection.query("INSERT INTO burgerTable (burger_name, devoured) VALUES (?, ?)", [req.body.author, false], function (
-    err,
-    result
-  ) {
-    if (err) {
-
-      return res.status(500).end();
-    }
-
-    res.json({ id: result.burger_name });
-  });
-});
-
-app.put("/api/burger/:id", function (req, res) {
-  console.log(req.body);
+app.post("/api/burger", function(req, res) {
   connection.query(
-    "UPDATE burgerTable SET devoured = true WHERE id = ?",
-    [req.body.id],
-    function (err, result) {
+    "INSERT INTO burgerTable (burger_name, devoured) VALUES (?, ?)",
+    [req.body.author, false],
+    function(err, result) {
       if (err) {
+        console.log(err);
 
         return res.status(500).end();
       }
-      else if (result.changedRows === 0) {
 
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
+      res.json({ id: result.burger_name });
     }
   );
 });
 
+app.put("/api/burger/:id", function(req, res) {
+  console.log(req.body);
+  connection.query(
+    "UPDATE burgerTable SET devoured = true WHERE id = ?",
+    [req.body.id],
+    function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      } else if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
